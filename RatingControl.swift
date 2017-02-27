28eee5e9f7526f1cc14599653c4f7b0e2cf32a7a
Henry Tung <cscyhenry@gmail.com>
2017-02-27 10:38:00 +0800

    added red square button

---
+++ RatingControl.swift
@@ -0,0 +1,45 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Henry Tung on 24/2/2017.
+//  Copyright © 2017 Henry Tung. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    //MARK: init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtons()
+    }
+
+    //MARK: Private Methods
+    private func setupButtons() {
+        let button = UIButton()
+        button.backgroundColor = UIColor.red
+        
+        //Add constraints
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        //Add the button to the stack
+        addArrangedSubview(button)
+    }
+}
